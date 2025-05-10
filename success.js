ce281f3a91108d5bf023549d38fc42756b9c838c
marocrubz <83349963+marocrubz@users.noreply.github.com>
2025-05-10 17:52:07 +0100

    Delete success.js

--- success.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-
-document.getElementById("userName").innerText = urlParams.get("name") || "N/A";
-document.getElementById("clockinTime").innerText = urlParams.get("time") || "N/A";
-document.getElementById("clockinDate").innerText = urlParams.get("date") || "N/A";
-document.getElementById("meetingType").innerText = urlParams.get("meetingType") || "N/A";
