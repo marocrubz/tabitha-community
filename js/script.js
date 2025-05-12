9031259068119d34272465b82fc60846bd5ab903
marocrubz <83349963+marocrubz@users.noreply.github.com>
2025-05-12 11:20:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 const API_URL = "https://tabitha-community.onrender.com/api/attendance";
 
+// loader.js
+const loaderHTML = `
+  <div id="page-loader">
+    <div class="spinner"></div>
+  </div>
+  <style>
+    #page-loader {
+      position: fixed;
+      z-index: 9999;
+      background-color: rgba(0, 0, 0, 0.5);
+      backdrop-filter: blur(8px);
+      -webkit-backdrop-filter: blur(8px);
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .spinner {
+      width: 60px;
+      height: 60px;
+      border: 4px solid rgba(102, 18, 18, 0.3);
+      border-top-color: red;
+      border-radius: 50%;
+      animation: spin 1s linear infinite;
+    }
+    @keyframes spin {
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  </style>
+`;
+
 
 function submitAttendance(event) {
     event.preventDefault(); // Prevent form from submitting normally
