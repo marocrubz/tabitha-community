43339bca0e2106ba689a91536960418b9165a919
marocrubz <83349963+marocrubz@users.noreply.github.com>
2025-05-17 20:27:15 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
   location: String,
 });
 
