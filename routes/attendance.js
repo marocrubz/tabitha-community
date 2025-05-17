b929b9cd3423aa9844a674084aaaac132b3f80d4
marocrubz <83349963+marocrubz@users.noreply.github.com>
2025-05-17 20:25:29 +0100

    Update attendance.js

--- routes/attendance.js
+++ routes/attendance.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.post('/', async (req, res) => {
-  const { name } = req.body;
+  const { name, meetingType, location } = req.body;
 
   if (!name) return res.status(400).json({ message: "Name is required" });
 
-  const existing = await User.findOne({ name, timestamp: { $gte: new Date().setHours(0, 0, 0, 0) } });
+  const existing = await User.findOne({ name, meetingType, location, timestamp: { $gte: new Date().setHours(0, 0, 0, 0) } });
   if (existing) {
     return res.status(409).json({ message: "Already clocked in today." });
   }
 
-  const newUser = new User({ name });
+  const newUser = new User({ name, meetingType, location });
   await newUser.save();
   res.status(201).json({ message: "Attendance recorded" });
 });
