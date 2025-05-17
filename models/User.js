f7feef919e29dba09858137d7eaf252a4936a234
marocrubz <83349963+marocrubz@users.noreply.github.com>
2025-05-17 20:26:54 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   timestamp: { type: Date, default: Date.now }
+  location: String,
 });
 
 module.exports = mongoose.model('User', UserSchema);
